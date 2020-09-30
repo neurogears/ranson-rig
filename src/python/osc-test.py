f2745767e089907836f88d8ca195bc2055f97ce3
glopesdev <glopesdev@users.noreply.github.com>
2020-09-30 16:22:22 +0100

    Add helper methods for #2

--- src/python/osc-test.py
+++ src/python/osc-test.py
@@ -6,6 +6,8 @@ Created on Thu Aug 13 11:44:13 2020
 """
 
 import rigstim
+from rigstim import WallType as Wall
+from rigstim import TriggerType as Trigger
 from pythonosc.udp_client import SimpleUDPClient
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import BlockingOSCUDPServer
@@ -33,24 +35,31 @@ with BlockingOSCUDPServer((ip, reply_port), dispatcher) as server:
     rig.gratings(size=30, x=-15, y=-5, angle=0, freq=0.1, duration=2.0) # grating 1
     rig.gratings(size=15, x=15, y=-5, angle=45, freq=0.1, duration=2.0, speed=1) # grating 2
     rig.video("Blink", y=20, speed=2.0, onset=1.0, duration=2.0) # video 1
-    client.send_message("/start", 0)
+    rig.start()
     server.handle_request()  # Wait for end trial
     
     rig.gratings(size=120, angle=30, freq=0.1, duration=2.0) # go gratings
-    client.send_message("/go", [30.0, 2.0, 0.5]) # go trial
+    rig.go(0.5) # go trial
     server.handle_request()  # Wait for end trial
 
     rig.gratings(size=120, angle=0, freq=0.1, duration=2.0) # nogo gratings
-    client.send_message("/nogo", [0.0, 2.0, 0.5]) # no-go trial
+    rig.nogo(0.5) # no-go trial
     server.handle_request()  # Wait for end trial
 
-    client.send_message("/tile", [0, 0.0, 1.0, "White", 7, 2.0, 1])
-    client.send_message("/tile", [1, 0.0, 1.0, "Black", 2, 3000.0, 3])
-    client.send_message("/tile", [0, 1.0, 1.0, "Black", 4, 3.0, 3])
-    client.send_message("/tile", [1, 1.0, 1.0, "White", 6, 1000.0, 2])
-    client.send_message("/tile", [0, 2.0, 1.0, "White", 1, 2000.0, 1])
-    client.send_message("/tile", [1, 2.0, 1.0, "Black", 3, 0.0, 2])
-    client.send_message("/tile", [2, 1.0, 1.0, "Black", 5, 0.1, 1])
-    client.send_message("/tile", [4, 3.0, 1.0, "mask", 0, 0.0, 0])
-    client.send_message("/startcorridor", [3.0, 1.2, 1.0, 0.0, 0.0, 0.0])
+    rig.tile(Wall.LEFT, position=0, extent=1, texture="White",
+             interaction=Trigger.REWARD_ON_LICK, argument=2, repetitions=1) 
+    rig.tile(Wall.RIGHT, position=0, extent=1, texture="Black",
+             interaction=Trigger.END_ON_LICK, argument=3000, repetitions=3)
+    rig.tile(Wall.LEFT, position=1, extent=1, texture="Black",
+             interaction=Trigger.TELEPORT_ON_LICK, argument=3.0, repetitions=3)
+    rig.tile(Wall.RIGHT, position=1, extent=1, texture="White",
+             interaction=Trigger.REWARD_ON_ENTRY, argument=1000.0, repetitions=2)
+    rig.tile(Wall.LEFT, position=2, extent=1, texture="White",
+             interaction=Trigger.END_ON_ENTRY, argument=2000.0)
+    rig.tile(Wall.RIGHT, position=2, extent=1, texture="Black",
+             interaction=Trigger.TELEPORT_ON_ENTRY, repetitions=2)
+    rig.tile(Wall.TOP, position=1, extent=1, texture="Black",
+             interaction=Trigger.CHANGE_GAIN_ON_ENTRY, argument=0.1)
+    rig.tile(Wall.FRONT, position=3, texture="mask")
+    rig.corridor(length=3.0, width=1.2, height=1.0, position=0.0)
     server.handle_request()  # Wait for end trial
--- src/python/rigstim.py
+++ src/python/rigstim.py
@@ -5,6 +5,25 @@ Created on Wed Sep 23 20:44:52 2020
 @author: gonca
 """
 
+from enum import Enum
+
+class WallType(Enum):
+    LEFT = 0
+    RIGHT = 1
+    TOP = 2
+    BOTTOM = 3
+    FRONT = 4
+    
+class TriggerType(Enum):
+    NONE = 0
+    END_ON_ENTRY = 1
+    END_ON_LICK = 2
+    TELEPORT_ON_ENTRY = 3
+    TELEPORT_ON_LICK = 4
+    CHANGE_GAIN_ON_ENTRY = 5
+    REWARD_ON_ENTRY = 6
+    REWARD_ON_LICK = 7
+
 class RigClient:
     def __init__(self, client):
         self.client = client
@@ -47,3 +66,41 @@ class RigClient:
                                  [[angle,width,height,x,y],
                                   [loop,speed,name],
                                   [onset,duration]])
+    
+    def start(self):
+        self.client.send_message("/start", 0)
+    
+    def go(self, duration):
+        self.client.send_message("/go", [duration])
+        
+    def nogo(self, duration):
+        self.client.send_message("/nogo", [duration])
+    
+    def tile(self, wall, **kwargs):
+        position = float(kwargs.get('position',0.0))
+        extent = float(kwargs.get('extent',1.0))
+        texture = str(kwargs.get('texture',"Transparent"))
+        interaction = kwargs.get('interaction',TriggerType.NONE)
+        argument = float(kwargs.get('argument',0.0))
+        repetitions = int(kwargs.get('repetitions',1))
+        self.client.send_message("/tile",
+                                 [int(wall.value),
+                                  position,
+                                  extent,
+                                  texture,
+                                  int(interaction.value),
+                                  argument,
+                                  repetitions])
+    
+    def corridor(self, length, **kwargs):
+        width = float(kwargs.get('width',1.0))
+        height = float(kwargs.get('height',1.0))
+        x = float(kwargs.get('x',0.0))
+        y = float(kwargs.get('y',0.0))
+        position = float(kwargs.get('position',0.0))
+        self.client.send_message("/corridor",
+                                 [length,
+                                  width,
+                                  height,
+                                  x, y,
+                                  position])
